b7830434f1ba660685f7a180aa2ceeab45d061fe
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-17 19:03:31 -0500

    refactor express server to use with socketio

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('new web socket connection');
+});
+
+server.listen(port, () => {
   console.log(`up and running on : ${port}`);
 });
