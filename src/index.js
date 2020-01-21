ccd5f6342633d7b935264846cbf1fd1c1ae94340
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-21 12:27:34 -0500

    change message from a string to object using util func

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
 
+const { generateMessage } = require('./utils/messages');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -14,19 +16,20 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicDirectoryPath));
 
 io.on('connection', socket => {
-  socket.broadcast.emit('message', 'New user joined!');
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('New user joined!'));
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
       return callback('Profanity is not a llowed');
     }
-    io.emit('message', message);
+    io.emit('message', generateMessage(message));
     callback();
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', 'User has left');
+    io.emit('message', generateMessage('User has left'));
   });
 
   socket.on('sendLocation', ({ longitude, latitude }, callback) => {
