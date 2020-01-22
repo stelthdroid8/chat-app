bd366967ed90bee557f8c875568d9fde7ed81a11
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-21 19:29:08 -0500

    using Qs lib, allow username and room to be kept track of

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
 
-const { generateMessage } = require('./utils/messages');
+const {
+  generateMessage,
+  generateLocationMessage
+} = require('./utils/messages');
+
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,26 +26,58 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicDirectoryPath));
 
 io.on('connection', socket => {
-  socket.emit('message', generateMessage('Welcome!'));
-  socket.broadcast.emit('message', generateMessage('New user joined!'));
+  socket.on('join', ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
+
+    if (error) {
+      return callback(error);
+    }
+    socket.join(user.room);
+
+    socket.emit('message', generateMessage('Admin', 'Welcome!'));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        generateMessage('Admin', `${user.username} has joined!`)
+      );
+
+    callback();
+    //socket.emit - specific client io.emit - every client socket.broadcast.emit - everyone besides this connection
+    //io.to.emit, socket.broadcast.to.emit -- these limit to specific chatrooms
+  });
+
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
       return callback('Profanity is not a llowed');
     }
-    io.emit('message', generateMessage(message));
+    io.to(user.room).emit('message', generateMessage(user.username, message));
     callback();
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', generateMessage('User has left'));
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        generateMessage('Admin', `${user.username} has left`)
+      );
+    }
   });
 
-  socket.on('sendLocation', ({ longitude, latitude }, callback) => {
-    io.emit(
+  socket.on('sendLocation', (coords, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
       'locationMessage',
-      `https://google.com/maps?q=${latitude},${longitude}`
+      generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+      )
     );
     callback();
   });
